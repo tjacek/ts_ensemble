7664135b401fdddf7ce9463d41e65ade33f1123b
tjacek <tjacek@agh.edu.pl>
2020-04-10 22:38:15 +0200

    subspace

--- bag.py
+++ bag.py
@@ -1,4 +1,4 @@
-import random
+import random,numpy as np
 import files,exp
 
 def make_bagset(in_path,out_path,k=7):
@@ -25,5 +25,20 @@ def train_bag(in_path,out_path,n_epochs=1000):
         out_i="%s/%s" % (out_path,in_i.split("/")[-1])
         exp.simple_exp(in_i,out_i,n_epochs=n_epochs)
 
-#make_bagset("agum","bagging",k=7)
-train_bag("bagging","bag_feats")
+def jackknife(in_path,out_path):
+    seq_dict=files.get_seqs(in_path)
+    dim=list(seq_dict.values())[0].shape[1]
+    X= np.array(list(seq_dict.values()))
+    names=list(seq_dict.keys())
+    files.make_dir(out_path)
+    for i in range(dim):
+        sub_i=np.delete(X,[i],axis=2)
+        out_i="%s/bag%d" %(out_path,i)
+        sub_dict_i={ name_j:sub_i[j]  
+                        for j,name_j in enumerate(names)}
+        files.save_seqs(sub_dict_i,out_i)
+    files.save_seqs(seq_dict,"%s/full" % out_path)
+
+#make_bagset("../MSR/agum","../MSR/bagging",k=7)
+train_bag("../MSR/subspace","../MSR/sub_feats")
+#jackknife("../MSR/agum","../MSR/subspace")
