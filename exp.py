8402d1e7bf06e1f10da761ce288efac11243a8d7
tjacek <tjacek@agh.edu.pl>
2020-10-13 20:05:19 +0900

    unify exp

--- exp.py
+++ exp.py
@@ -2,11 +2,23 @@ import preproc,frame,files
 import spline,block,bag
 import dtw
 
-def dtw_exp(in_path,feat_type):
+def dtw_exp(in_path,feat_type,out=False):
     files.make_dir(feat_type)
     seq_path="%s/seqs" % feat_type
-    frame.compute(in_path,seq_path,feat_set=feat_type)
+    frame.compute(in_path,seq_path,
+        feat_set=feat_type,out=out)
     dtw.make_dtw_feats(feat_type)
+
+def unify_exp(paths,out_path,filename="dtw"):
+    paths=["%s/%s" % (path_i,filename) 
+            for path_i in paths]
+    files.unify_feats(paths,out_path)
+#    feats=[]
+#    for path_i in paths:
+#        in_i="%s/%s" % (path_i,filename)
+#        feat_i=files.get_feats(in_i)
+#        feats.append(feat_i)
+
 
 def scaled_exp(in_path,out_path):
     files.make_dir(out_path)
@@ -33,6 +45,6 @@ def exp(in_path,out_path):
     feat_path=out_path+"/feats"
     bag.train_bag(bag_path,feat_path,n_epochs=1000)
 
-#exp("../MSR/box","simple")
-#smooth_exp("../MSR/box","gauss")
-dtw_exp("box","skew")
+if __name__ == '__main__':
+#dtw_exp("box","skew",out=True)
+    unify_exp(["max_z","skew","corl"],"full2",filename="feats")
