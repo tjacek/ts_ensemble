103b6b25acb026822968bb435d4e21db609cd2f3
tjacek <tjacek@agh.edu.pl>
2020-11-12 16:46:21 +0100

    sigma normalization

--- preproc.py
+++ preproc.py
@@ -19,19 +19,29 @@ def gauss_helper(img_i):
 
 def norm_seqs(in_path,out_path):
 	seqs=files.get_seqs(in_path)
-	seq_max=np.array([np.amax(seq_i,axis=0)
-				for name_i,seq_i in seqs.items()])
-	seq_max= np.amax(seq_max,axis=0)
+	fun= sigma_norm(seqs)
 	new_seqs={}
 	for name_i,seq_i in seqs.items():
-		seq_i=[  ts_j/seq_max[j] 
+		seq_i=[ fun(ts_j,j) 
 				for j,ts_j in enumerate(seq_i.T)]
-		seq_i=np.array(seq_i)
+		seq_i=np.array(seq_i).T
 		new_seqs[name_i]=seq_i
 	files.make_dir(out_path)
 	for name_i,seq_i in new_seqs.items():
 		out_i="%s/%s" % (out_path,name_i)
 		np.save(out_i,seq_i)
 
+def norm_max(seqs):
+	seq_max=np.array([np.amax(seq_i,axis=0)
+				for name_i,seq_i in seqs.items()])
+	seq_max=np.amax(seq_max,axis=0)
+	return lambda ts_j,j: ts_j/seq_max[j]
+
+def sigma_norm(seqs):
+	all_seqs=np.concatenate(list(seqs.values()),axis=0)
+	mean_seq=np.mean(all_seqs,axis=0)
+	std_seq=np.std(all_seqs,axis=0)
+	return lambda ts_j,j: (ts_j-mean_seq[j])/std_seq[j]
+
 if __name__=="__main__":
-    norm_seqs("skeleton/seqs","new_seqs")
+    norm_seqs("skeleton/seqs","skeleton2/seqs")
