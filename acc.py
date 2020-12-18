739bece231ca9fc8f40693374ce4b5a657b5ba5e
tjacek <tjacek@agh.edu.pl>
2020-12-18 15:29:00 +0100

    select classifers

--- acc.py
+++ acc.py
@@ -2,23 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import voting,train
 
-def acc_exp(common_path,binary_path,binary,out_path):
+def acc_exp(common_path,binary_path,binary,out_path=None):
     acc=acc_curve(common_path,binary_path,binary)
     print(acc)
-    print(acc[7])
     if(out_path):
         np.savetxt(out_path,acc)
     show_curve(acc,name="acc_curve",out_path=None)
 
-def acc_curve(common_path,binary_path=None,binary=False):
-	datasets=voting.read_datasets(common_path,binary_path)
-	acc=voting.cross_acc(datasets)
-	datasets=[ datasets[i] for i in np.argsort(acc)]
-	results=[ train.train_model(data_i,binary)
-				for data_i in datasets]
-	acc=[voting.voting(results[:k+1]).get_acc() 
-			for k in range(len(results))]
-	return acc
+def acc_curve(common_path,binary_path,binary=False,acc_only=True):
+    datasets=voting.read_datasets(common_path,binary_path)
+    acc=voting.cross_acc(datasets)
+    datasets=[ datasets[i] for i in np.argsort(acc)]
+    results=[ train.train_model(data_i,binary)
+                for data_i in datasets]
+    acc=[voting.voting(results[:k+1])
+    for k in range(len(results))]
+    if(acc_only):
+        acc=[ result_i.get_acc() for result_i in acc]
+    return acc
+
+def selected_clf(common_path,binary_path,binary,k,out_path):
+    result=acc_curve(common_path,binary_path,binary,acc_only=False)
+    print(result[k].get_acc())
+    print(result[k].get_cf(out_path))
 
 def show_curve(acc,name="acc_curve",out_path=None):
     if(not name):
@@ -36,9 +42,11 @@ def show_curve(acc,name="acc_curve",out_path=None):
     return acc
 
 if __name__ == "__main__":
-	common_path=['../3DHOI/dtw/max_z/person',
-             '../3DHOI/dtw/corl/person']
-#             '../agum/skew/person']
-	acc_exp(common_path,'../3DHOI/ens/feats',False,'acc/3DHOI')
-#	print(acc)
-	
+    dataset="MHAD"
+    common_path=['../%s/dtw/max_z/person' % dataset,
+                '../%s/dtw/corl/person' % dataset]
+    binary_path='../%s/ens/feats' % dataset
+#    acc_exp(common_path,binary_path,False)
+    k=11
+    out_path="cf%s" % dataset
+    selected_clf(common_path,binary_path,False,k,out_path)
